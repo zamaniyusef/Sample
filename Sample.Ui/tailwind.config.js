2cf7a4746dc41dff1773b9ef131e86b2451f97ac
Yousef Zamani <zamaniyusef@hotmail.com>
2023-02-19 16:45:01 +0330

    feat: init tailwind

---
+++ Sample.Ui/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.html',
+    './**/*.razor'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
